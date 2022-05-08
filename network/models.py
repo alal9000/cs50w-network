2cfcc0ac5abc30b53c4ece25e46250699ec56462
Aaron <aaronlal72@gmail.com>
2022-05-08 10:20:28 +1000

    update

--- network/models.py
+++ network/models.py
@@ -4,21 +4,25 @@ from django.db import models
 
 
 class User(AbstractUser):
-    following = models.IntegerField(default=0)
-    followers = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.username
-
+    pass
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.TextField(blank=False,)
     date = models.DateTimeField(auto_now_add=True)
-    likes = models.ManyToManyField(User, blank=True, related_name='user_like')
+    likes = models.ManyToManyField(User, blank=True, related_name='like_user')
 
     def __str__(self):
         return self.post
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    follower = models.ManyToManyField(User, blank=True, related_name='follower_user')
+    following = models.ManyToManyField(User, blank=True, related_name='following_user')
+
+    def __str__(self):
+        return self.user.username
 
 
 
